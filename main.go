1072826f6ddf4f93dc078b03f5722e7d9387a857
cylmsun <jiajia.sun@pacteraedge.com>
2023-04-21 14:57:15 +0800

    commit

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+func getRepeatCharIndex(str string) int {
+	hMap := make(map[int32]int)
+	for i, c := range str {
+		hMap[c-'a']++
+		fmt.Printf("   过程1: %d,%d,%d \n\n", hMap[c], c, i)
+	}
+	for i, c := range str {
+		fmt.Printf("   过程2: %d,%d,%d \n\n", hMap[c], c, i)
+		if hMap[c-'a'] == 1 {
+			return i
+		}
+	}
+	return -1
+}
+
 func main() {
-	//fmt
+	index := getRepeatCharIndex("aabb")
+	fmt.Printf("结果: %d \n\n", index)
 }
